29cc43689ed4da5220014ee2bea831793d09d166
Clayton Peterson <clayton.peterson@maine.edu>
2014-04-06 20:05:20 -0400

    Got most of the PacaCollection class finished. It now creates an array list of alpacas by fetching the information from the database. There is a info method, which takes the string name of an alpaca and returns information about it.

--- src/alpaca/Alpaca.java
+++ src/alpaca/Alpaca.java
@@ -4,10 +4,42 @@ import PacaTraca.PacaTracaImpl;
 public class Alpaca {
 	
 	public PacaTracaImpl hardware;
+	private float longitude;
+	private float latitude;
+	private String id;
 	
-	public Alpaca (String SensorID)
+	public Alpaca ()
 	{
-		hardware = new PacaTracaImpl(SensorID);
+		hardware = new PacaTracaImpl ();
 	}
 	
+	public void setID (String id)
+	{
+		this.id = id;
+	}
+	
+	public void setLongitude (float longitude)
+	{
+		this.longitude = longitude;
+	}
+	
+	public void setLatitude (float latitude)
+	{
+		this.latitude = latitude;
+	}
+	
+	public String getID ()
+	{
+		return this.id;
+	}
+	
+	public float getLongitude ()
+	{
+		return this.longitude;
+	}
+	
+	public float getLatitude ()
+	{
+		return this.latitude;
+	}
 }
--- src/alpaca/PacaCollection.java
+++ src/alpaca/PacaCollection.java
@@ -1,24 +1,102 @@
 package alpaca;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import alpaca.Alpaca;
 
 public class PacaCollection {
 	
-	private Firebase ref;
+	// URL and reference to the Firebase database
+	private String   url;
+	private Firebase dataRef;
+	
+	// The working collection of alpacas
+	private ArrayList <Alpaca> alpacas;
 	
 	public PacaCollection ()
 	{
-		 ref = new Firebase ("https://crackling-fire-2064.firebaseio.com/alpacas");
-		 System.out.println (ref);
+		alpacas = new ArrayList <Alpaca> ();
+		
+		url = "https://crackling-fire-2064.firebaseio.com/alpacas";
+		dataRef = new Firebase (url);
+		
+		dataRef.addChildEventListener (new ChildEventListener()
+		{
+		    @Override
+		    public void onChildAdded (DataSnapshot snapshot, String previousChildName)
+		    {
+		        HashMap childValues = (HashMap) snapshot.getValue ();
+		        Double longitude    = (Double) childValues.get ("lng");
+		        Double latitude     = (Double) childValues.get ("lat");		        
+		        String name         = (String) childValues.get ("name");
+		        
+		        // Set the alpaca object with info we received from the DB
+		        Alpaca in = new Alpaca ();
+		        in.setLongitude(longitude.floatValue());
+		        in.setLatitude (latitude.floatValue());
+		        in.setID (name);
+		        
+		        alpacas.add (in);
+		        info (name);
+		    }
+
+		    @Override
+		    public void onChildChanged(DataSnapshot snapshot, String previousChildName) 
+		    {
+
+		    }
+
+		    @Override
+		    public void onChildRemoved(DataSnapshot snapshot)
+		    {
+
+		    }
+
+		    @Override
+		    public void onChildMoved(DataSnapshot snapshot, String previousChildName)
+		    {
+
+		    }
+
+			@Override
+			public void onCancelled(FirebaseError arg0) 
+			{
+				System.out.println ("out");	
+			}
+		});		
+	}
+	
+	public void info (String id)
+	{
+		Alpaca info = get (id);
+		if (info != null)
+		{
+			System.out.println ("ID: " + info.getID());
+			System.out.println ("Longitude: " + info.getLongitude());
+			System.out.println ("Latitude: " + info.getLatitude());
+		}
+		System.out.println ();
+	}
+	
+	public Alpaca get (String id)
+	{
+		for (Alpaca a : alpacas)
+		{
+			if (a.getID () == id)
+				return a;
+		}
+		return null;
 	}
 	
 	public void add (String key, Alpaca alpaca)
-	{
-		
+	{	
+		dataRef.push ().setValue (alpaca);
 	}
 	
 	public void remove ()
@@ -26,27 +104,9 @@ public class PacaCollection {
 		
 	}
 	
-	public Alpaca getAlpaca (Integer key)
-	{
-		return null;
-	}
-	
-	public void eraseCollection ()
-	{
-		
-	}
-	
-	public boolean contains (Integer key)
-	{
-		return false;
-	}
-	
 	public static void main (String[] args) 
 	{
 		new PacaCollection ();
-		System.out.println ("hi");
 	}
 }
-
-
 
