b2840b71790cccbfbb3f975a3a2216a4dcfbba96
Jake Marsh <jakemmarsh@gmail.com>
2014-04-08 09:53:17 -0400

    changed alpaca service to a factory so all controllers are working off of the same instance

--- public/js/services/alpacas.js
+++ public/js/services/alpacas.js
@@ -1,7 +1,7 @@
 define(['./index'], function (services) {
   'use strict';
   // expand input and show post button on focus
-  services.service('alpacaService', ['$firebase',  function($firebase) {
+  services.factory('alpacaService', ['$firebase',  function($firebase) {
     var ref = new Firebase('https://crackling-fire-2064.firebaseio.com/alpacas');
     return $firebase(ref);
   }]);
