8f1562759d39d239b079f206933dc01c12bfc64d
Jake Marsh <jakemmarsh@gmail.com>
2014-04-13 18:23:47 -0400

    finished frontend alerts logic

--- public/js/controllers/inner-ctrl.js
+++ public/js/controllers/inner-ctrl.js
@@ -2,8 +2,18 @@ define(['./index'], function (controllers) {
     'use strict';
     controllers.controller('innerCtrl', ['$scope', '$rootScope', '$location', 'alertService', function ($scope, $rootScope, $location, alertService) {
 
-        $rootScope.alerts = alertService.getAlerts();
-        console.log($rootScope.alerts);
+        $rootScope.alerts = alertService;
+
+        alertService.$on("change", function() {
+            var keys = alertService.$getIndex();
+            $rootScope.numAlerts = 0;
+
+            for(var i = 0; i < keys.length; i++) {
+                if(alertService[keys[i]].read === false) {
+                    $rootScope.numAlerts += 1;
+                }
+            }
+        });
 
         $scope.logout = function() {
             $rootScope.user = null;
